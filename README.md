7c1d6b3364b66ae2c760639cdc0a8cb66c401c34
rawikumarp <iamrawikumar@gmail.com>
2018-03-04 17:03:13 +0530

    latest files committed

--- README.md
+++
@@ -1,2 +0,0 @@
-# buildpipeline
-This is for buildpipeline
---
+++ src/main/java/com/buildpipe/sample/buildpipe/buildpipe.java
@@ -0,0 +1,13 @@
+package com.buildpipe.sample.buildpipe;
+
+/**
+ * Hello buildpipeline!
+ *
+ */
+public class buildpipe 
+{
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello buildpipeline!" );
+    }
+}
